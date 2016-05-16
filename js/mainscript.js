66ce897c6d0817d31786b6ffbb7c06a8209c3d01
SofaKing18 <sofakingworld@gmail.com>
2016-05-16 22:35:33 +0300

    formated code

--- js/mainscript.js
+++ js/mainscript.js
@@ -1,26 +1,22 @@
-var colorCounter = 0;
+var colorCounter=0;
 
 function LoadImageAndResizeCanvas() {
 var img = new Image(); 
 img.crossOrigin = "Anonymous";
-img.src = prompt("Copy Image URL here:", "octopus.jpg");
+img.src = prompt("Copy Image URL here:", "example.jpg");
 img.onload = function() {
     var ctx = document.getElementById('canvas').getContext('2d');
     ctx.canvas.width = img.width;
     ctx.canvas.height = img.height;
     ctx.drawImage(this, 0, 0);
-   img.crossOrigin = '';
-  var div = document.createElement('div');
-  div.setAttribute("id", "colors");
-  div.setAttribute("color", "FFA");
-  div.setAttribute("width", "24px");
-  div.setAttribute("width", "50px");
-
-
-  div.innerHTML = "<input type='button' value = 'Add Color' id ='colorlist' onclick = 'addColor()'>"
-  document.body.insertBefore(div,document.getElementById('FIM'));
-  
-}   
+    var div = document.createElement('div');
+    div.setAttribute("id", "colors");
+    div.setAttribute("color", "FFA");
+    div.setAttribute("width", "24px");
+    div.setAttribute("width", "50px");
+    div.innerHTML = "<input type='button' value = 'Add Color' id ='colorlist' onclick = 'addColor()'>"
+    document.body.insertBefore(div,document.getElementById('FIM'));
+  }   
 }
 
 function CreateColorCanvas()
@@ -47,8 +43,8 @@ function move() {
     }
 }
 
-function addColor()
-{ colorCounter++;
+function addColor() { 
+  colorCounter++;
   var numberColor  = document.createElement('div');
   numberColor.setAttribute("id", colorCounter);
   numberColor.setAttribute("class", "numberColors");
@@ -57,50 +53,45 @@ function addColor()
   CreateColorCanvas();
 }
 
-function changingcolor(id,value)
-{
-var ctx = document.getElementById(id+'canvas').getContext('2d');
-var mctx = document.getElementById('canvas').getContext('2d');
-var imgData = mctx.getImageData(0, 0, mctx.canvas.width, mctx.canvas.height);
-var curColor = hexToRgbA(value).split(",");
-var quality = 40;//prompt("Quality:", "50");
-for (var i=0;i<imgData.data.length;i+=4)
-  {
-  if ((Math.abs(curColor[0]-imgData.data[i+0])<quality)&&(Math.abs(curColor[1]-imgData.data[i+1])<quality)&&(Math.abs(curColor[2]-imgData.data[i+2])<quality)){
-    imgData.data[i+0]=curColor[0];
-    imgData.data[i+1]=curColor[1];
-    imgData.data[i+2]=curColor[2];
-    imgData.data[i+3]=255;
-  }
-  else
-  {
-    imgData.data[i+0]=255;
-    imgData.data[i+1]=255;
-    imgData.data[i+2]=255;
-    imgData.data[i+3]=0;
-  }
-  }
-for (var i=imgData.width*4+1;i<imgData.data.length-imgData.width*4-1;i+=4){ 
-  if ((imgData.data[i-4+3]==0)&&(imgData.data[i+3]==255)&&(imgData.data[i+4+3]==0)){
-    imgData.data[i+0]=0;
-    imgData.data[i+1]=0;
-    imgData.data[i+2]=0;
-    imgData.data[i+3]=255; 
-}
-
-  }
-ctx.putImageData(imgData,0,0);
-}
-
-function hexToRgbA(hex){
-    var c;
-    if(/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)){
-        c= hex.substring(1).split('');
-        if(c.length== 3){
-            c= [c[0], c[0], c[1], c[1], c[2], c[2]];
-        }
-        c= '0x'+c.join('');
-        return [(c>>16)&255, (c>>8)&255, c&255].join(',')+',1';
+function changingcolor(id,value) {
+  var ctx = document.getElementById(id+'canvas').getContext('2d');
+  var mctx = document.getElementById('canvas').getContext('2d');
+  var imgData = mctx.getImageData(0, 0, mctx.canvas.width, mctx.canvas.height);
+  var curColor = hexToRgbA(value).split(",");
+  var quality = 40;//prompt("Quality:", "50");
+  for (var i=0;i<imgData.data.length;i+=4) {
+    if ((Math.abs(curColor[0]-imgData.data[i+0])<quality)&&(Math.abs(curColor[1]-imgData.data[i+1])<quality)&&(Math.abs(curColor[2]-imgData.data[i+2])<quality)) {
+      imgData.data[i+0]=curColor[0];
+      imgData.data[i+1]=curColor[1];
+      imgData.data[i+2]=curColor[2];
+      imgData.data[i+3]=255;
+    } else {
+      imgData.data[i+0]=255;
+      imgData.data[i+1]=255;
+      imgData.data[i+2]=255;
+      imgData.data[i+3]=0;
     }
-    throw new Error('Bad Hex');
+  }
+  for (var i=imgData.width*4+1;i<imgData.data.length-imgData.width*4-1;i+=4){ 
+    if ((imgData.data[i-4+3]==0)&&(imgData.data[i+3]==255)&&(imgData.data[i+4+3]==0)){
+      imgData.data[i+0]=0;
+      imgData.data[i+1]=0;
+      imgData.data[i+2]=0;
+      imgData.data[i+3]=255; 
+    }
+  }
+  ctx.putImageData(imgData,0,0);
+}
+
+function hexToRgbA(hex) {
+  var c;
+  if(/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)) {
+    c= hex.substring(1).split('');
+    if(c.length== 3){
+      c= [c[0], c[0], c[1], c[1], c[2], c[2]];
+    }
+    c= '0x'+c.join('');
+    return [(c>>16)&255, (c>>8)&255, c&255].join(',')+',1';
+  }
+  throw new Error('Bad Hex');
 }
